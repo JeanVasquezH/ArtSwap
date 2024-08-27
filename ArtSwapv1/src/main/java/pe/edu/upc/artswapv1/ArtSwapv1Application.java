b05fb3afb848ecd8d22fea4498bba0aa25274035
JeanVasquezH <129628557+JeanVasquezH@users.noreply.github.com>
2024-08-26 20:55:23 -0500

    Add files via upload

---
+++ ArtSwapv1/src/main/java/pe/edu/upc/artswapv1/ArtSwapv1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.artswapv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtSwapv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtSwapv1Application.class, args);
+    }
+
+}
