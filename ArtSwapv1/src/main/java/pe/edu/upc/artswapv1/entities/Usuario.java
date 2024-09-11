260e58e5e459ee088984c14c8277b3a7f7c040b9
Eloy <eloy.alatago@udlap.mx>
2024-09-11 01:59:12 -0500

    corregir

--- ArtSwapv1/src/main/java/pe/edu/upc/artswapv1/entities/Usuario.java
+++ ArtSwapv1/src/main/java/pe/edu/upc/artswapv1/entities/Usuario.java
@@ -45,59 +45,5 @@ public class Usuario {
         rolUser = rolusuario;
     }
 
-    public int getIdUsuario() {
-        return IdUsuario;
-    }
 
-    public void setIdUsuario(int idUsuario) {
-        IdUsuario = idUsuario;
-    }
-
-    public String getCorreo_Electronico() {
-        return Correo_Electronico;
-    }
-
-    public void setCorreo_Electronico(String correo_Electronico) {
-        Correo_Electronico = correo_Electronico;
-    }
-
-    public String getNombre() {
-        return Nombre;
-    }
-
-    public void setNombre(String nombre) {
-        Nombre = nombre;
-    }
-
-    public String getContrasena() {
-        return Contrasena;
-    }
-
-    public void setContrasena(String contrasena) {
-        Contrasena = contrasena;
-    }
-
-    public Date getFecha_Registro() {
-        return Fecha_Registro;
-    }
-
-    public void setFecha_Registro(Date fecha_Registro) {
-        Fecha_Registro = fecha_Registro;
-    }
-
-    public Comunidad getComu() {
-        return comu;
-    }
-
-    public void setComu(Comunidad comu) {
-        this.comu = comu;
-    }
-
-    public Rolusuario getRolUser() {
-        return rolUser;
-    }
-
-    public void setRolUser(Rolusuario rolUser) {
-        this.rolUser = rolUser;
-    }
 }
