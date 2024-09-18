c65810d866d55709a349140b871e3253ced7122e
Kevinski2004 <kevinayquipa5@gmail.com>
2024-09-18 00:41:17 -0500

    Correcciones
    Remplazo la version anterior

--- ArtSwapv1/src/main/java/pe/edu/upc/artswapv1/repositories/IFavoritosRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.artswapv1.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.artswapv1.entities.Favoritos;
-
-public interface IFavoritosRepository extends JpaRepository<Favoritos, Integer> {
-}
--- ArtSwapv1/src/main/java/pe/edu/upc/artswapv1/repositories/ISubastaRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.artswapv1.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.artswapv1.entities.Subasta;
-
-public interface ISubastaRepository extends JpaRepository<Subasta, Integer> {
-}
