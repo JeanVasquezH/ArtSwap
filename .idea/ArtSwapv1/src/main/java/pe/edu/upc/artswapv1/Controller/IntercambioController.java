03a700f78b69d68aaaff163c4fd5d7bdb7c8a29c
DJSantos52 <delossantosd1502@gmail.com>
2024-09-20 03:47:24 -0500

    correciones

---
+++ .idea/ArtSwapv1/src/main/java/pe/edu/upc/artswapv1/Controller/IntercambioController.java
@@ -0,0 +1,32 @@
+package pe.edu.upc.artswapv1.Controller;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.artswapv1.dtos.IntercambioDTO;
+import pe.edu.upc.artswapv1.entities.Intercambio;
+import pe.edu.upc.artswapv1.serviceinterfaces.IIntercambioService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("intercambios")
+public class IntercambioController {
+    @Autowired
+    private IIntercambioService iS;
+
+    @PostMapping
+    public void insertar(@RequestBody IntercambioDTO dto){
+        ModelMapper m=new ModelMapper();
+        Intercambio mn=m.map(dto,Intercambio.class);
+        iS.insert(mn);
+    }
+    @GetMapping
+    public List<IntercambioDTO> listar(){
+        return iS.list().stream().map(x->{
+            ModelMapper m=new ModelMapper();
+            return m.map(x,IntercambioDTO.class);
+        }).collect(Collectors.toList());
+    }
+}
---
+++ .idea/ArtSwapv1/src/main/java/pe/edu/upc/artswapv1/Controller/RolUsuarioController.java
@@ -0,0 +1,55 @@
+package pe.edu.upc.artswapv1.Controller;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.artswapv1.dtos.RolUsuarioDTO;
+import pe.edu.upc.artswapv1.entities.Rolusuario;
+import pe.edu.upc.artswapv1.serviceinterfaces.IRolUsuarioService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/RolUsuario")
+public class RolUsuarioController {
+    @Autowired
+    private IRolUsuarioService rU;
+
+    //Seguridad
+    @PostMapping
+    public void registrar(@RequestBody RolUsuarioDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Rolusuario r = m.map(dto, Rolusuario.class);
+        rU.insert(r);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Long id) {
+        rU.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public RolUsuarioDTO listarId(@PathVariable("id") Long id) {
+        ModelMapper m = new ModelMapper();
+        RolUsuarioDTO dto = m.map(rU.listId(id), RolUsuarioDTO.class);
+        return dto;
+    }
+
+    //
+
+    @PutMapping
+    public void modificar(@RequestBody RolUsuarioDTO dto){
+        ModelMapper m=new ModelMapper();
+        Rolusuario d=m.map(dto, Rolusuario.class);
+        rU.update(d);
+    }
+
+    @GetMapping
+    public List<RolUsuarioDTO> listar(){
+        return rU.list().stream().map(x->{
+            ModelMapper m=new ModelMapper();
+            return m.map(x, RolUsuarioDTO.class);
+        }).collect(Collectors.toList());
+    }
+}
