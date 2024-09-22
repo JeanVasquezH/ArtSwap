591dc16750eddbae567b456d768a31f484b5cdda
Kevinski2004 <kevinayquipa5@gmail.com>
2024-09-22 17:55:47 -0500

    Implementacion de cloud
    we use cloud

--- .idea/ArtSwapv1/src/main/java/pe/edu/upc/artswapv1/repositories/IIntercambioRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.artswapv1.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.artswapv1.entities.Intercambio;
-
-@Repository
-public interface IIntercambioRepository extends JpaRepository<Intercambio, Integer> {
-}
