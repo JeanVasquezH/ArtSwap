1dc261260478603af7b2730937176ce68f4cce43
Agustin <129628557+JeanVasquezH@users.noreply.github.com>
2024-09-27 00:33:36 -0500

    delete all

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM amazoncorretto:21-alpine-jdk
-MAINTAINER ESMERALDA
-COPY target/ArtSwapv1-0.0.1-SNAPSHOT.jar app.jar
-ENTRYPOINT ["java", "-jar","/app.jar"]
--- src/main/java/pe/edu/upc/artswapv1/repositories/IVentaRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.artswapv1.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.artswapv1.entities.Venta;
-
-@Repository
-public interface IVentaRepository extends JpaRepository<Venta, Integer> {
-}
